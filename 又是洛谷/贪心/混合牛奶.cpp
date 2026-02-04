39ea9da3acc7af2c64c853ad844360e618b16bef
calistalearns <1615699158@qq.com>
2026-02-04 17:40:38 +0800

    a

--- 又是洛谷/贪心/混合牛奶.cpp
+++ 又是洛谷/贪心/混合牛奶.cpp
@@ -1,7 +1,35 @@
 # include <iostream>
+# include <vector>
+# include <algorithm>
 using namespace std;
+
+struct M
+{
+    int one,num;//单价和数量
+};
+
 int main()
 {
-    
+    int n,m;//需求和人数
+    cin>>n>>m;
+
+    vector<M>arr(m);
+    for(int i=0;i<m;i++)
+    {
+        cin>>arr[i].one>>arr[i].num;
+    }
+    sort(arr.begin(),arr.end(),[](M a,M b)
+    {
+        return a.one<b.one;//单价小的在前
+    });
+    int spend=0;
+    int result=n;
+    for(int i=0;i<n&&result>0;i++)
+    {
+        int buy=min(arr[i].num,result);
+        spend+=arr[i].one*buy;
+        result-=arr[i].num;
+    }
+    cout<<spend<<endl;
     return 0;
 }
