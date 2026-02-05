2d1324d8b37214002502b33aa7352e67c41fc1fd
calistalearns <1615699158@qq.com>
2026-02-05 22:26:39 +0800

    a

--- 又是洛谷/贪心/拼数.cpp
+++ 又是洛谷/贪心/拼数.cpp
@@ -1,7 +1,27 @@
 # include <iostream>
+# include <string>
+# include <vector>
+# include <algorithm>
 using namespace std;
 int main()
 {
-    
-    return;
+    int n;
+    cin>>n;
+    vector<string> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr.begin(),arr.end(),[](const string &a,const string &b)
+    {
+        return a+b>b+a;
+    });
+    string result;
+    for(int i=0;i<n;i++)
+    {
+        result+=arr[i];
+    }
+    cout<<result<<endl;
+    return 0;
 }
