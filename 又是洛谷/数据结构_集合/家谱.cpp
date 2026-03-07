5693baa83dc1f02c2b9a8c2f4e951d3308bcad9c
calistalearns <1615699158@qq.com>
2026-03-07 16:33:31 +0800

    b

--- 又是洛谷/数据结构_集合/家谱.cpp
+++ 又是洛谷/数据结构_集合/家谱.cpp
@@ -1,8 +1,40 @@
 # include <iostream>
 # include <string>
+# include <map>
 using namespace std;
+char zf;
+string s,s1;
+map<string,string>m;
+string find(string a)
+{
+    if(a!=m[a]) return find(m[a]);
+    else return a;
+}
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>zf;
+    while(zf!='$')
+    {
+        cin>>s;
+        if(zf=='#')//父亲
+        {
+            s1=s;
+            if(m.find(s) == m.end()) m[s] = s;
+        }
+        else if(zf=='+')//儿子
+        {
+            if(m.find(s) == m.end()) m[s] = s;
+            m[s]=s1;
+        }
+        else if(zf=='?')//找祖先
+        {
+            string f=find(s);
+            cout<<s<<" "<<f<<endl;
+        }
+        cin>>zf;
+    }
     
     return 0;
 }
