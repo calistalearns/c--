927d59ff11978ba371431a853aa8718384cf9597
calistalearns <1615699158@qq.com>
2026-03-25 14:36:29 +0800

    c

--- 五十/求区间和.cpp
+++ 五十/求区间和.cpp
@@ -1,9 +1,21 @@
 # include <iostream>
 using namespace std;
 const int N=1e5+10;
-int n,m;
+int n,m,l,r;
+int f[N],a[N];
 int main()
 {
-    cin>>n>>m;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        f[i]=f[i-1]+a[i];
+    }
+    cin>>m;
+    while(m--)
+    {
+        cin>>l>>r;
+        cout<<f[r]-f[l-1]<<endl;
+    }
     return 0;
 }
