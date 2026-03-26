3a9b1f14d0f0f1daa7a531d217cd14dc2207c979
calistalearns <1615699158@qq.com>
2026-03-26 22:04:09 +0800

    d

--- 五十/k倍区间.cpp
+++ 五十/k倍区间.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 const int N=1e5+10;
-int n,k;
+typedef long long ll;
+int n,k,cnt[N];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    cin>>n>>k;
+    ll ans=0,sum=0;
+    cnt[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        int x;cin>>x;
+        sum=(sum+x)%k;
+        ans+=cnt[sum];
+        cnt[sum]++;
+    }
+    cout<<ans<<endl;
     return 0;
 }
