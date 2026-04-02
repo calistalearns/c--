df3492fa0a82ca5dbca77040ea67a10081b7a83a
calistalearns <1615699158@qq.com>
2026-04-02 18:56:56 +0800

    e

---
+++ 西八真题_蓝桥杯/2022/修建灌木.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<max(n-i,i-1)*2<<endl;
+    }
+    return 0;
+}
